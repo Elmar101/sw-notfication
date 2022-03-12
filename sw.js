40ed171359a8bbb64e280dc3f703431002450743
Elmar Amanov <Elmar.amanov10@gmail.com>
2022-03-12 14:43:28 +0400

    5 update

--- sw.js
+++ sw.js
@@ -5,7 +5,9 @@ self.addEventListener("push",(event)=>{
         data: {
             dateOfArrival: Date.now(),
             primaryKey: "4"
-        }
+        },
+        icon: "images/logo.png",
+        vibrate: [100, 50, 100],
     } 
     event.waitUntil(self.registration.showNotification('Yeni Meqale Elave Edildi', config));
 })
